adbab2e3a77bd8efb648a4bae3d4bd0b9063a4f8
Charlie Nguyen <43255516+charlien1@users.noreply.github.com>
2024-05-17 03:54:57 -0700

    Updated link to repository.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/charlien1'
+      href='https://github.com/charlien1/charlieportfolio'
       className='link footer__link'
     >
       Created By Charlie Nguyen
