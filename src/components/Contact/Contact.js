c310004fe3cf4fe4647a75b42fdcc46334223ebb
Charlie Nguyen <43255516+charlien1@users.noreply.github.com>
2024-05-17 03:41:36 -0700

    Updated contact info.

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
   return (
     <section className='section contact center' id='contact'>
       <h2 className='section__title'>Contact Me</h2>
-      <span className='section__subtitle'>657-340-0499 | charlienguyen688@yahoo.com</span>
+      <span className='section__subtitle'>Phone: 657-340-0499 | Email: charlienguyen688@yahoo.com | Discord: chaarii</span>
       <br /><a href={`mailto:${contact.email}`}>
         <span type='button' className='btn btn--outline'>
           Email Me
